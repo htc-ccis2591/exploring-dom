6074ca862842b36b562c447f8b0795b7bb7aedb7
Mypeterhub <petersrun@yahoo.com>
2015-09-15 13:20:03 -0500

    Exp-dom review

--- js/script.js
+++ js/script.js
@@ -4,24 +4,28 @@
 
 //---------------------------------------------
 // Create a variable to hold the requested item and log it to the console
+var item = document.getElementsByTagName("h1");
+console.log(item);
 
 // 1.  The heading that says "Weekly Specials"
+var headings = document.getElementsByTagName("h2").item(0); console.log(headings);
 
 
-// 2.  The next sibling of the item from #1.
-
+// 2.  The next sibling of the item from #1. 
+var headings = document.getElementsByTagName("h2").item(1); console.log(headings);
 
 // 3.  The first child of the item from #2.
-
+var headings = document.getElementsByTagName("h2").item(2); console.log(headings);
 
 //  4. The item with the id of "move".
 
 
-//  5. The last child of the item from #4.
 
+//  5. The last child of the item from #4.
+var headings = document.getElementsByTagName("h2").item(4); console.log(headings);
 
 //  6. All of the paragraphs that are children of the item from #4.
-
+var paragraphs = document.getElementsByTagName("p").item(4); console.log(paragraphs);
 
 //  7. The previous sibling of the item from #4.
 
